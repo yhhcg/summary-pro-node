d1893837fb3e8265c14a29abd32f95b3a762465f
余珩 <1281005689@qq.com>
2018-10-21 20:43:26 +0800

    [core] Body-parse middleware, set response header and write login api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var logger = function(req, res, next) {
   console.log('LOGGER');
@@ -7,13 +8,29 @@ var logger = function(req, res, next) {
 }
 
 app.use(logger);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
+app.use(bodyParser.json());
+// Parse text/plain
+app.use(bodyParser.text());
+
+app.use(function(req, res, next) {
+  // Allow code from any origin to access a resource
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.post('/login', function (req, res) {
   console.log('POST');
   console.log(req.body);
-  res.send('success');
+  console.log(req.query);
+  res.status(200).json({
+    errorCode: '0',
+    errMessage: 'ok',
+  });
 });
-
+99
 app.listen(3000, function () {
   console.log('Listening on port 3000!');
 });
